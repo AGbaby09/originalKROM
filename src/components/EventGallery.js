cc1d482f267b676f0ccdea6e85794257e2c32597
Agbotse Selasi Michael <agbotsem@gmail.com>
2023-05-04 03:50:39 +0000

    commit

--- src/components/EventGallery.js
+++ src/components/EventGallery.js
@@ -1,6 +1,7 @@
-import Obiaa from '../assets/Obiaa.MP4';
+// import Obiaa from '../assets/Obiaa.MP4';
 import TWM from '../assets/TWM.mp4';
 import TWM1 from '../assets/TWM1.mp4';
+import TWM2 from '../assets/TWM3.mp4';
 import VideoEvG from "./VideoEvG";
 const EventsGallery = () => {
     return ( 
@@ -12,10 +13,10 @@ const EventsGallery = () => {
                 <div id="eventRightTop">
                     <VideoEvG vidURL={TWM1}></VideoEvG>
                 </div>
-                <div id="eventRightCenter">
-                </div>
+                {/* <div id="eventRightCenter">
+                </div> */}
                 <div id="eventRightBottom">
-                    <VideoEvG vidURL={Obiaa}></VideoEvG>
+                    <VideoEvG vidURL={TWM2}></VideoEvG>
                 </div>
             </div>
         </div>
